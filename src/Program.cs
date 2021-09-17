d734abcce3cde0996936546c9563f57000ccc274
Michael Ostermeier <mosterme@users.noreply.github.com>
2021-09-17 10:34:26 +0200

    Created a very basic screensaver.

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,48 @@
-﻿using System;
-
+﻿using System.Windows.Forms;
+using System;
 namespace awesomescr
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine("Awesome Screensaver");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            if (args.Length > 0)
+            {
+                if (args[0].ToLower().Trim().Substring(0, 2) == "/s") //show
+                {
+                    //show the screen saver
+                    ShowScreenSaver();
+                    Application.Run();
+                }
+                else if (args[0].ToLower().Trim().Substring(0, 2) == "/p") //preview
+                {
+                    //preview the screen saver
+                    IntPtr previewWndHandle = new IntPtr(long.Parse(args[1]));
+                    Application.Run(new Screensaver(previewWndHandle));
+                }
+                else if (args[0].ToLower().Trim().Substring(0, 2) == "/c") //configure
+                {
+                    //configure the screen saver
+                    // TODO
+                }
+            }
+            else
+            {
+                //no arguments were passed (we should probably show the screen saver)
+                ShowScreenSaver();
+                Application.Run();
+            }
+        }
+        static void ShowScreenSaver()
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                Screensaver screensaver = new Screensaver(screen.Bounds);
+                screensaver.Show();
+            }
         }
     }
 }
