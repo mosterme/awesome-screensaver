84feadd10c92243a671403a66098a9e040ed7772
Michael Ostermeier <mosterme@users.noreply.github.com>
2021-09-22 12:57:40 +0200

    Refactored Settings to us tabs.

--- src/Settings.cs
+++ src/Settings.cs
@@ -26,12 +26,9 @@ namespace awesomescr
         }
         private void InitializeComponent()
         {
-            this.Height = 360;
-            this.Width = 240;
-            Button button1 = new Button();
-            Button button2 = new Button();
-            button1.Text = "OK";
-            button2.Text = "Cancel";
+            this.Height = 300;
+            this.Width = 225;
+
             Label label_acrylic = new Label();
             Label label_awesome = new Label();
             Label label_classic = new Label();
@@ -44,63 +41,94 @@ namespace awesomescr
             label_awesome.Text = "Font Awesome 4.7";
             label_classic.Text = "Smileys (classic)";
             label_kaomoji.Text = "Smileys (kaomoji)";
-            label_acrylic.Text  = "Use Acrylic";
             label_african.Text = "African Scripts";
             label_american.Text = "American Scripts";
             label_emoji.Text = "Emoji + Pictographs";
             label_maths.Text = "Mathematical Symbols";
             label_other.Text = "Other Symbols";
+            label_acrylic.Text  = "Enable Acrylic";
+
             label_awesome.Width = label_classic.Width = label_kaomoji.Width = label_acrylic.Width = 130;
             label_african.Width = label_american.Width = label_emoji.Width = label_maths.Width = label_other.Width = 130;
+
             label_awesome.Location = new Point(label_x, 10);
             font_awesome_47.Location = new Point(check_x, label_awesome.Location.Y);
             label_classic.Location = new Point(label_x, 40);
             smileys_classic.Location = new Point(check_x, label_classic.Location.Y);
             label_kaomoji.Location = new Point(label_x, 70);
             smileys_kaomoji.Location = new Point(check_x, label_kaomoji.Location.Y);
-            label_african.Location = new Point(label_x, 100);
+
+            label_african.Location = new Point(label_x, 10);
             unicode_african.Location = new Point(check_x, label_african.Location.Y);
-            label_american.Location = new Point(label_x, 130);
+            label_american.Location = new Point(label_x, 40);
             unicode_american.Location = new Point(check_x, label_american.Location.Y);
-            label_emoji.Location = new Point(label_x, 160);
+            label_emoji.Location = new Point(label_x, 70);
             unicode_emoji.Location = new Point(check_x, label_emoji.Location.Y);
-            label_maths.Location = new Point(label_x, 190);
+            label_maths.Location = new Point(label_x, 100);
             unicode_maths.Location = new Point(check_x, label_maths.Location.Y);
-            label_other.Location = new Point(label_x, 220);
+            label_other.Location = new Point(label_x, 130);
             unicode_other.Location = new Point(check_x, label_other.Location.Y);
-            label_acrylic.Location = new Point(label_x, 250);
+
+            label_acrylic.Location = new Point(label_x, 10);
             acrylic_enabled.Location = new Point(check_x, label_acrylic.Location.Y);
-            button1.Location = new Point(Width - button2.Width - button1.Width - 40, Height - button1.Height - 50);
-            button2.Location = new Point(Width - button2.Width - 30, button1.Location.Y);
+
+            TabControl tabControl1 = new TabControl();
+            tabControl1.Location = new Point(5,5);
+            tabControl1.Size = new Size(200, 200);
+            TabPage tabPage1 = new TabPage();
+            TabPage tabPage2 = new TabPage();
+            TabPage tabPage3 = new TabPage();
+
+            tabPage1.Text = "Awesome";
+            tabPage1.TabIndex = 0;
+            tabPage1.Controls.Add(label_awesome);
+            tabPage1.Controls.Add(font_awesome_47);
+            tabPage1.Controls.Add(label_classic);
+            tabPage1.Controls.Add(smileys_classic);
+            tabPage1.Controls.Add(label_kaomoji);
+            tabPage1.Controls.Add(smileys_kaomoji);
+
+            tabPage2.Text = "Unicode";
+            tabPage2.TabIndex = 1;
+            tabPage2.Controls.Add(label_african);
+            tabPage2.Controls.Add(unicode_african);
+            tabPage2.Controls.Add(label_american);
+            tabPage2.Controls.Add(unicode_american);
+            tabPage2.Controls.Add(label_emoji);
+            tabPage2.Controls.Add(unicode_emoji);
+            tabPage2.Controls.Add(label_maths);
+            tabPage2.Controls.Add(unicode_maths);
+            tabPage2.Controls.Add(label_other);
+            tabPage2.Controls.Add(unicode_other);
+
+            tabPage3.Text = "Screensaver ";
+            tabPage3.TabIndex = 2;
+            tabPage3.Controls.Add(label_acrylic);
+            tabPage3.Controls.Add(acrylic_enabled);
+
+            this.Controls.Add(tabControl1);
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+
+            Button button1 = new Button();
+            Button button2 = new Button();
+            button1.Text = "OK";
+            button2.Text = "Cancel";
+            button1.Location = new Point(Width - button2.Width - button1.Width - 35, Height - button1.Height - 50);
+            button2.Location = new Point(Width - button2.Width - 25, button1.Location.Y);
             button1.Click += new EventHandler(okButton_Click);
             button2.Click += new EventHandler(cancelButton_Click);
+
             this.Text = "Awesome Screensaver Settings";
+            this.AcceptButton = button1;
+            this.CancelButton = button2;
+            this.Controls.Add(button1);
+            this.Controls.Add(button2);
             this.FormBorderStyle = FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.AcceptButton = button1;
-            this.CancelButton = button2;
             this.StartPosition = FormStartPosition.CenterScreen;
-            this.Controls.Add(label_awesome);
-            this.Controls.Add(font_awesome_47);
-            this.Controls.Add(label_classic);
-            this.Controls.Add(smileys_classic);
-            this.Controls.Add(label_kaomoji);
-            this.Controls.Add(smileys_kaomoji);
-            this.Controls.Add(label_acrylic);
-            this.Controls.Add(acrylic_enabled);
-            this.Controls.Add(label_african);
-            this.Controls.Add(unicode_african);
-            this.Controls.Add(label_american);
-            this.Controls.Add(unicode_american);
-            this.Controls.Add(label_emoji);
-            this.Controls.Add(unicode_emoji);
-            this.Controls.Add(label_maths);
-            this.Controls.Add(unicode_maths);
-            this.Controls.Add(label_other);
-            this.Controls.Add(unicode_other);
-            this.Controls.Add(button1);
-            this.Controls.Add(button2);
         }
         private void okButton_Click(object sender, EventArgs e)
         {
