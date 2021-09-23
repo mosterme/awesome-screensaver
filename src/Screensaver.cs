b90c494e5597f220cacd102ab4a97515b8a4286c
Michael Ostermeier <mosterme@users.noreply.github.com>
2021-09-23 14:55:32 +0200

    Added East Asian Scripts.

--- src/Screensaver.cs
+++ src/Screensaver.cs
@@ -89,13 +89,14 @@ namespace awesomescr
             if (settings.font_awesome_47.Checked) providers.Add(new Awesome());
             if (settings.smileys_classic.Checked) providers.Add(new Smileys(Smileys.classic));
             if (settings.smileys_kaomoji.Checked) providers.Add(new Smileys(Smileys.kaomoji));
+            if (settings.unicode_emoji.Checked) providers.Add(new Unicode(Unicode.emoji_pictographs));
+            if (settings.unicode_maths.Checked) providers.Add(new Unicode(Unicode.mathematical_symbols));
+            if (settings.unicode_other.Checked) providers.Add(new Unicode(Unicode.other_symbols));
             if (settings.unicode_african.Checked) providers.Add(new Unicode(Unicode.african_scripts));
             if (settings.unicode_american.Checked) providers.Add(new Unicode(Unicode.american_scripts));
-            if (settings.unicode_emoji.Checked) providers.Add(new Unicode(Unicode.emoji_pictographs));
+            if (settings.unicode_e_asian.Checked) providers.Add(new Unicode(Unicode.east_asian_scripts));
             if (settings.unicode_oceania.Checked) providers.Add(new Unicode(Unicode.indonesia_oceania_scripts));
-            if (settings.unicode_maths.Checked) providers.Add(new Unicode(Unicode.mathematical_symbols));
             if (settings.unicode_middle.Checked) providers.Add(new Unicode(Unicode.middle_eastern_scripts));
-            if (settings.unicode_other.Checked) providers.Add(new Unicode(Unicode.other_symbols));
             this.provider = randomProvider();
             this.components = new System.ComponentModel.Container();
             this.AutoScaleDimensions = new SizeF(6F, 13F);
